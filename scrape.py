1a3b93e658cc60613135b7a3653d83f7887006eb
leesouthwart <leesouthwart@gmail.com>
2021-01-17 19:56:47 +0000

    refactor: Data coming from Json. Allowing for multiple set names. Put loop into function

--- scrape.py
+++ scrape.py
@@ -1,41 +1,44 @@
-import requests
 from bs4 import BeautifulSoup
-import time
-import random
+import time, random, json, requests
 
-vividData = {
-    'Allister-V3': 4,
-    'Rayquaza-Amazing-Burst': 35,
-    'Bea-V3': 37
-}
+f = open('data.json',)
+data = json.load(f)
 
-for key in vividData:
-    item = key
-    buyPrice = round(vividData[key] * 1.12, 2) #Multiply to get EUR price
-    
 
-    # Build Url
-    vividURL = 'https://www.cardmarket.com/en/Pokemon/Products/Singles/Vivid-Voltage/' + item + '?language=1&minCondition=2'
+def checkPrice(setName):
+    for key in data[setName]:
+        val = data[setName][key]
+        item = key
+        buyPrice = round(val * 1.12, 2) #Multiply to get EUR price
+        
 
-    #Setup and scrape data
-    headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
-    page = requests.get(vividURL, headers=headers)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    container = soup.find('div', {'class': 'price-container'})
+        # Build Url
+        vividURL = 'https://www.cardmarket.com/en/Pokemon/Products/Singles/' + setName + '/' + item + '?language=1&minCondition=2'
 
-    #Format price
-    price = container.span.text
-    price = price.replace(',','.')
-    price = float(price[:-2])
+        #Setup and scrape data
+        headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
+        page = requests.get(vividURL, headers=headers)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        container = soup.find('div', {'class': 'price-container'})
 
-    #Calculate difference between price and buy price
-    difference = buyPrice - price
-    difference = round(difference, 2)
+        #Format price
+        price = container.span.text
+        price = price.replace(',','.')
+        price = float(price[:-2])
 
-    if price <= buyPrice:
-        print('Price for item "' + item + '" is lower than its buy price by ' + str(difference) + '. Currently listed at ' + str(price) + '.')
-    else:
-        print('No deals found for "' + item + '".')
+        #Calculate difference between price and buy price
+        difference = buyPrice - price
+        difference = round(difference, 2)
 
-    # Lets not get IP banned or stress their servers
-    time.sleep(random.randint(30, 60))
+        if price <= buyPrice:
+            print('Price for item "' + item + '" is lower than its buy price by ' + str(difference) + '. Currently listed at ' + str(price) + '.')
+        else:
+            print('No deals found for "' + item + '".')
+
+        # Lets not get IP banned or stress their servers
+        time.sleep(random.randint(15, 45))
+
+    f.close()
+
+checkPrice('rebel-clash')
+checkPrice('vivid-voltage')
